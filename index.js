62a62914c2fa5af22b9a07a412d41b2016b658e6
Jordan Capa <jcapasix@everis.com>
2019-08-23 13:14:36 -0500

    update default port comment

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
 });
 
-POST method route
 app.get('/apple-app-site-association', function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify(json));
