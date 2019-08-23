8610fd1270b4705dc9b69539508a68c367653770
Jordan Capa <jcapasix@everis.com>
2019-08-23 13:23:16 -0500

    x

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ app.get('/', function (req, res) {
   res.send('udeep: Universal Links');
 });
 
-app.listen(5000, function () {
+app.get('/apple-app-site-association', function (req, res) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(json));
+  //res.send('POST request to the homepage');
+});
+
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 5000!');
 });
 
-// app.get('/apple-app-site-association', function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'application/json'});
-//   res.end(JSON.stringify(json));
-//   //res.send('POST request to the homepage');
-// });
+
